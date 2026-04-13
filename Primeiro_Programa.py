fb99859ad05d5b280ba0225e4d08287671de4e79
RF-Gody <rafaelffgodoy@gmail.com>
2026-04-13 19:25:58 -0300

    App de criação de senhas
    esse aplicativo gera senhas com base no tamanho da senha que você que quer e em todas os caracteres da senha

---
+++ Primeiro_Programa.py
@@ -0,0 +1,8 @@
+import random
+elements = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+pass_length = int(input("insira o comprimento da senha"))
+
+password = ""
+for i in range(pass_length):
+    password += random.choice(elements)
+print(password)
